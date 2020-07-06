b872d3c52749fa9cfedec3b1046aca7db3cbfead
WASHINGTON LUIS DE ASSIS PEREIRA <slash.o.teobaldo@gmail.com>
2020-07-06 15:54:18 -0300

    inserindo pause caso abra o exe

--- buscar.py
+++ buscar.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import os
 
 palavra = input("digite o que você busca:")
 
@@ -10,21 +11,20 @@ driver.get('https://www.google.com.br/')
 
 driver.find_element(By.NAME,"q").send_keys(palavra)
 time.sleep(1)
-
 while True:
-    print(" busca com sorte digite 1 , busca normal digite 2 , 3 para buscar outra vez")
+    print(" busca com sorte digite 1 , busca normal digite 2 , 3 para buscar outra vez ou 4 para sair")
     buscaq = int(input("Digite a opção:"))
     time.sleep(1)
     if buscaq == 1:
         driver.find_element(By.NAME,"btnI").click()
     if buscaq ==2:
         driver.find_element(By.NAME,"btnK").click()
-    elif buscaq == 3:
+    if buscaq == 3:
         palavra = input("digite o que você busca:")
         driver.get('https://www.google.com.br/')
         driver.find_element(By.NAME, "q").send_keys(palavra)
-    else:
+    elif buscaq == 4:
         driver.close()
         break
 
-
+os.system("pause")
